8b02b59b748d2991d73e24f3da3aa50825be4fa4
Praveen-pk01 <85781381+Praveen-pk01@users.noreply.github.com>
2021-06-23 07:43:30 +0530

    Add files via upload

---
+++ day 10-op.py
@@ -0,0 +1,14 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: D:\Users\PRAVEEN\AppData\Local\Programs\Python\Python39\test.py ===
+Hello!!! Welcome to the Deposit & Withdrawal Machine
+Enter amount to be Deposited: 1500
+
+ Amount Deposited: 1500.0
+Enter amount to be Withdrawn: 750.20
+
+ You Withdrew: 750.2
+
+ Net Available Balance= 749.8
+>>> 
